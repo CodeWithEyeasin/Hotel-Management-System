b3009b0da66b420e969e12b4435717297f59fbdf
Md. Eyeasin Arafat <mreyeasinarafat@gmail.com>
2025-02-06 11:59:28 +0600

    add carousel, login,registration

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <style>
         *{
             font-family: 'Poppins', sans-serif;
@@ -14,9 +15,17 @@
         .h-font{
             font-family: 'Merienda', cursive;
         }
+            input::-webkit-outer-spin-button,
+            input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+            }
+            input[type=number] {
+            -moz-appearance: textfield;
+            }
     </style>
 </head>
-<body>
+<body class="bg-light">
     
     <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
@@ -94,19 +103,54 @@
                 <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-            <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control shadow-none">
+            <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                Note: Your details must match with your ID (National ID card, Passport, Driving license, etc.)
+                that will be required during check-in.
+            </span>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 ps-0 mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 p-0 mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 ps-0 mb-3">
+                        <label class="form-label">Phone Number</label>
+                        <input type="number" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 p-0 mb-3">
+                        <label class="form-label">Picture</label>
+                        <input type="file" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-12 p-0 mb-3">
+                        <label class="form-label">Address</label>
+                        <textarea class="form-control shadow-none" rows="1"></textarea>
+                    </div>
+                    <div class="col-md-6 ps-0 mb-3">
+                        <label class="form-label">Pincode</label>
+                        <input type="number" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 p-0 mb-3">
+                        <label class="form-label">Date of birth</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 ps-0 mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control shadow-none">
+                    </div>
+                    <div class="col-md-6 p-0 mb-3">
+                        <label class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control shadow-none">
+                    </div>
+                </div>
             </div>
-            <div class="mb-4">
-                <label class="form-label">Password</label>
-                <input type="password" class="form-control shadow-none">
-            </div>
-            <div class="d-flex align-items-center justify-content-between mb-2">
-                <button type="submit" class="btn btn-dark shadow-none">
-                    LOGIN
+            <div class="text-center my-1">
+            <button type="submit" class="btn btn-dark shadow-none">
+                    REGISTER
                 </button>
-                <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password</a>
             </div>
             </div>
             </form>
@@ -114,7 +158,49 @@
         </div>
     </div>
 
+    <!-- Carousel -->
 
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <div class="container-fluid px-lg-4 mt-4">
+        <div class="swiper swiper-container">
+        <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="images/carousel/1.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+            <img src="images/carousel/2.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+            <img src="images/carousel/3.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+            <img src="images/carousel/4.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+            <img src="images/carousel/5.png" class="w-100 d-block" />
+        </div>
+        <div class="swiper-slide">
+            <img src="images/carousel/6.png" class="w-100 d-block" />
+        </div>
+        </div>
+        </div>
+    </div>
+
+    <!-- Check availability form -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <script>
+        var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        effect: "fade",
+        loop: true,
+        autoplay:{
+            delay: 3500,
+            disableOnInteraction: false,
+        }
+        });
+    </script>
+
 </body>
 </html>
